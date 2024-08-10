2165841b6da9e19f98ab848561d241beee6d6500
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-10 13:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
 
+document.addEventListener('DOMContentLoaded',()=>{
+	const infList = document.querySelector('#infi-list');
+
+	funstion addItems(num){
+		for(let i=0; i<num; i++){
+			const li = document.createElement('li');
+			li.textContent = `Item ${infList.children.length + 1}`;
+			infList.appendChild(li);
+		}
+	}
+	addItems(10);
+
+	function handleScroll(){
+		const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
+		if(scrollTop+clientHeight>= scrollHeight-5){
+			addItems(2);
+		}
+	}
+	window.addEventListener('scroll', handleScroll);
+});
